6f72f7893bc3706d0aed26fb67a88046465dec6a
vyrtoq <sha256@vyrtoq.xyz>
2024-01-20 01:49:31 +0100

    Solve up until if + q1 and q2 (done by mistake)

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(n_apples: i32) -> i32 {
+    if n_apples > 40 {
+        n_apples
+    } else {
+        n_apples * 2
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,11 +32,24 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(&str, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()), // .to_uppercase() returns a String
+                Command::Trim => output.push(string.trim().to_string()), // .trim() returns a &str
+                // cant concatenate a &str and a String
+                Command::Append(reps) => output.push(string.to_string() + &"bar".repeat(*reps)),
+                // Command::Append(reps) => {
+                    // let mut bars = String::new();
+                    // for i in 0..*reps {
+                    //     bars.push_str("bar");
+                    // }
+                    // output.push(string.to_string() + &bars);
+                //}
+            }
         }
         output
     }
@@ -45,7 +58,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]

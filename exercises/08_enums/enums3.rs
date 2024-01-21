7c9e254639dec93708dc2c2d6cc82c8bf4ab2a82
vyrtoq <sha256@vyrtoq.xyz>
2024-01-21 15:48:15 +0100

    Solve up until quiz2 (excluded)

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -5,10 +5,11 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit
 }
 
 struct Point {
@@ -41,9 +42,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
-        // Remember: When passing a tuple as a function argument, you'll need extra parentheses:
-        // fn function((t, u, p, l, e))
+        match message {
+            Message::ChangeColor(r, g, b) => State::change_color(self, (r, g, b)),
+            Message::Echo(s) => State::echo(self, s),
+            Message::Move(p) => State::move_position(self, p),
+            Message::Quit => State::quit(self),
+        }
     }
 }
 
--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,19 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_string() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars","balloons")
 }
 
 #[cfg(test)]
@@ -30,11 +28,11 @@ mod tests {
         assert_eq!(trim_me("  What's up!"), "What's up!");
         assert_eq!(trim_me("   Hola!  "), "Hola!");
     }
-
+    
     #[test]
     fn compose_a_string() {
-        assert_eq!(compose_me("Hello"), "Hello world!");
         assert_eq!(compose_me("Goodbye"), "Goodbye world!");
+        assert_eq!(compose_me("Hello"), "Hello world!");
     }
 
     #[test]
